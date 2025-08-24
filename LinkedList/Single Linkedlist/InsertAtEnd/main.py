daddaf5297a492f20b0fd067e6eb2c54fa79ef84
Immu <shaikimam37302@gmail.com>
2025-08-24 18:52:59 +0530

    Basic Linkedlist Operations

---
+++ LinkedList/Single Linkedlist/InsertAtEnd/main.py
@@ -0,0 +1,37 @@
+class Node:
+    def __init__(self,val):
+        self.data=val
+        self.addr=None
+
+class Linkedlist:
+    def __init__(self):
+        self.head=None
+
+    def insertatend(self,val):
+        newNode=Node(val)
+
+        if self.head is None:
+            self.head=newNode
+        else:
+            temp = self.head
+            while temp.addr is not None:
+                temp=temp.addr
+            temp.addr=newNode
+            
+    def display(self):
+        if self.head is None:
+            print("Linkedlist is Empty")
+        temp=self.head
+        while temp is not None:
+            print(temp.data,end=" ")
+            temp=temp.addr
+
+ll=Linkedlist()
+val=0
+
+while True:
+    val=int(input())
+    if(val==-1):
+        break
+    ll.insertatend(val)
+ll.display()
