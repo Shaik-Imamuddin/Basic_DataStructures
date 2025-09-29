b0b01a8fd6fde1ea4beec87c6feb246fa7aa6e53
Immu <shaikimam37302@gmail.com>
2025-09-29 13:32:47 +0530

    Basic Linkedlist Operations

---
+++ LinkedList/Double Linkedlist/InsertAtEnd/Main.java
@@ -0,0 +1,82 @@
+import java.util.Scanner;
+
+class Node {
+    int data;
+    Node prev;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        prev = null;
+        next = null;
+    }
+}
+
+class DoublyLinkedList {
+    private Node head;
+
+    public DoublyLinkedList() {
+        head = null;
+    }
+
+    public void insertAtEnd(int val) {
+        Node newNode = new Node(val);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node temp = head;
+            while (temp.next != null) { 
+                temp = temp.next;
+            }
+            temp.next = newNode;
+            newNode.prev = temp;
+        }
+    }
+
+    public void displayForward() {
+        if (head == null) {
+            System.out.println("List is Empty");
+            return;
+        }
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public void displayBackward() {
+        if (head == null) {
+            System.out.println("List is Empty");
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null) { 
+            temp = temp.next;
+        }
+        while (temp != null) { 
+            System.out.print(temp.data + " ");
+            temp = temp.prev;
+        }
+        System.out.println();
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        DoublyLinkedList dll = new DoublyLinkedList();
+
+        int val;
+        do {
+            val = sc.nextInt();
+            if (val != -1) {
+                dll.insertAtEnd(val);
+            }
+        } while (val != -1);
+
+        dll.displayForward();
+        dll.displayBackward();
+    }
+}
