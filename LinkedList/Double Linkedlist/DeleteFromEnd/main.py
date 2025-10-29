39c31183d14eb2984dc0736c0fd954c59141dfea
Immu <shaikimam37302@gmail.com>
2025-10-29 15:47:35 +0530

    Basic Linkedlist Operations

---
+++ LinkedList/Double Linkedlist/DeleteFromEnd/main.py
@@ -0,0 +1,69 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.prev = None
+        self.next = None
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insertAtEnd(self, val):
+        new_node = Node(val)
+        if self.head is None:
+            self.head = new_node
+        else:
+            temp = self.head
+            while temp.next is not None:  
+                temp = temp.next
+            temp.next = new_node
+            new_node.prev = temp
+
+    def deleteFromEnd(self):
+        if self.head is None:
+            print("Linkedlist is Empty")
+            return
+
+        if self.head.next is None:
+            self.head = None
+            return
+
+        temp = self.head
+        while temp.next is not None:  
+            temp = temp.next
+    
+        temp.prev.next = None  
+
+    def displayForward(self):
+        if self.head is None:
+            print("Linkedlist is Empty")
+            return
+        temp = self.head
+        while temp is not None:
+            print(temp.data, end=" ")
+            temp = temp.next
+        print()
+
+    def displayBackward(self):
+        if self.head is None:
+            print("Linkedlist is Empty")
+            return
+        temp = self.head
+        while temp.next is not None:
+            temp = temp.next
+        while temp is not None: 
+            print(temp.data, end=" ")
+            temp = temp.prev
+        print()
+
+dll = DoublyLinkedList()
+
+while True:
+    val = int(input())
+    if val==-1:
+        break
+    dll.insertAtEnd(val)
+
+dll.deleteFromEnd()
+dll.displayForward()
+dll.displayBackward()
