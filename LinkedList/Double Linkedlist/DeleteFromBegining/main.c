f285a0049f8d4d8b83585c9f129bfaf47972da08
Immu <shaikimam37302@gmail.com>
2025-10-30 19:09:45 +0530

    Basic Linkedlist Operations

---
+++ LinkedList/Double Linkedlist/DeleteFromBegining/main.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *prev;
+    struct Node *next;
+};
+struct Node *head = NULL;
+
+void insertAtEnd(int val) {
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+    newNode->data = val;
+    newNode->next = NULL;
+    newNode->prev = NULL;
+
+    if(head == NULL){
+        head = newNode;
+    } else{
+        struct Node *temp = head;
+        while(temp->next != NULL){
+            temp = temp->next;
+        }
+        temp->next = newNode;
+        newNode->prev = temp;
+    }
+}
+
+void deleteFromBeginning(){
+    if (head == NULL){
+        printf("Linkedlist is Empty");
+        return;
+    }
+    if(head->next == NULL){ 
+        free(head);
+        head = NULL;
+    }else{
+        struct Node* temp = head;
+        head = head->next;
+        head->prev = NULL;
+        free(temp);
+    }
+}
+
+void displayForward(){
+    if (head == NULL){
+        printf("Linkedlist is Empty\n");
+        return;
+    }
+    struct Node *temp = head;
+    while(temp != NULL){
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+void displayBackward(){
+    if (head == NULL){
+        printf("Linkedlist is Empty\n");
+        return;
+    }
+    struct Node *temp = head;
+    while (temp->next != NULL){ 
+        temp = temp->next;
+    }
+    while (temp != NULL){
+        printf("%d ", temp->data);
+        temp = temp->prev;
+    }
+    printf("\n");
+}
+
+int main(){
+    int val;
+    do{
+        scanf("%d", &val);
+        if (val != -1)
+            insertAtEnd(val);
+    } while (val != -1);
+    deleteFromBeginning();
+    displayForward();
+    displayBackward();
+    return 0;
+}
