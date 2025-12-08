ccdf2fd652115302d48bf06d1718c1dc8f75dcf2
Immu <shaikimam37302@gmail.com>
2025-12-08 09:18:02 +0530

    Basic Linkedlist Operations

--- LinkedList/Circular Linkedlist/Singly_Circular_Linkedlist/main.cpp
+++ LinkedList/Circular Linkedlist/Singly_Circular_Linkedlist/main.cpp
@@ -1,52 +1,81 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-struct Node{
+class Node{
+public:
     int data;
-    Node* addr;
+    Node *addr;
+
+    Node(int data){
+        this->data = data;
+        addr = nullptr;
+    }
 };
 
-Node* head = NULL;
+class Linkedlist{
+private:
+    Node *head;
+public:
+    Linkedlist(){
+        head = nullptr;
+    }
 
-void insertAtEnd(int val){
-    Node* newNode = new Node();
-    newNode->data = val;
-    newNode->addr = NULL;
+    void insertatend(int val){
+        Node *newNode = new Node(val);
 
-    if (head == NULL){
-        head = newNode;
-        newNode->addr = head;
-    } else{
-        Node* temp = head;
-        while (temp->addr != head){
-            temp = temp->addr;
+        if(head == nullptr){
+            head = newNode;
+            newNode->addr = head; 
+        } 
+        else{
+            Node *temp = head;
+
+            while(temp->addr != head){
+                temp = temp->addr;
+            }
+
+            temp->addr = newNode;
+            newNode->addr = head; 
         }
-        temp->addr = newNode;
-        newNode->addr = head;
     }
-}
 
-void display(){
-    if (head == NULL){
-        cout << "List is Empty\n";
-        return;
+    void display(){
+        if(head==nullptr){
+            cout<<"List is Empty\n";
+            return;
+        }
+        Node *temp = head;
+        do{
+            cout<<temp->data<<" ";
+            temp = temp->addr;
+        }while(temp!=head);
+        cout<<endl;
     }
-    Node* temp = head;
-    do{
-        cout<<temp->data<<" ";
-        temp=temp->addr;
-    }while(temp!=head);
-    cout<<endl;
-}
+
+    ~Linkedlist(){
+        if(head==nullptr) 
+            return;
+
+        Node *temp = head->addr;
+
+        while(temp!=head){
+            Node *nextNode = temp->addr;
+            delete temp;
+            temp = nextNode;
+        }
+        delete head;
+        head = nullptr;
+    }
+};
 
 int main(){
+    Linkedlist ll;
     int val;
-
     do{
         cin>>val;
-        if (val != -1)
-            insertAtEnd(val);
-    }while(val != -1);
-    display();
+        if(val!=-1)
+            ll.insertatend(val);
+    }while(val!=-1);
+    ll.display();
     return 0;
 }
