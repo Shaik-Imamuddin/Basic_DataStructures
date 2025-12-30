a279349d6a04b041cc77d6fd483015f0cd5d4eca
Immu <shaikimam37302@gmail.com>
2025-12-30 09:52:41 +0530

    Basic Queue Operations

---
+++ Queue/Circular_Queue/main.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node *addr;
+    Node(int val){
+        data=val;
+        addr=NULL;
+    }
+};
+
+Node *front=NULL;
+Node *rear=NULL;
+
+void enqueue(int val){
+    Node *newNode=new Node(val);
+    if(front==NULL){
+        front=rear=newNode;
+        rear->addr=front;
+    }else{
+        rear->addr=newNode;
+        rear=newNode;
+        rear->addr=front;
+    }
+}
+
+void dequeue(){
+    if(front==NULL){
+        cout<<"Queue Underflow!\n";
+        return;
+    }
+    if(front==rear){
+        cout<<front->data<<"\n";
+        delete front;
+        front=rear=NULL;
+    }else{
+        Node *temp=front;
+        cout<<temp->data<<"\n";
+        front=front->addr;
+        rear->addr=front;
+        delete temp;
+    }
+}
+
+void peek(){
+    if(front==NULL){
+        cout<<"Queue is empty!\n";
+        return;
+    }
+    cout<<"Front element: "<<front->data<<"\n";
+}
+
+void display(){
+    if(front==NULL){
+        cout<<"Queue is empty!\n";
+        return;
+    }
+    Node *temp=front;
+    do{
+        cout<<temp->data<<" ";
+        temp=temp->addr;
+    }while(temp!=front);
+    cout<<"\n";
+}
+
+int main(){
+    int val;
+    do{
+        cin>>val;
+        if(val!=-1)
+            enqueue(val);
+    }while(val!=-1);
+
+    display();
+    dequeue();
+    peek();
+    display();
+    return 0;
+}
